69cafd63bc6804b106723895f09498e96f725076
Zorglube <zorglube@broncotoxique.com>
2021-10-05 15:41:02 +0200

    Formating

--- src/main/java/io/github/deweyjose/graphqlcodegen/Codegen.java
+++ src/main/java/io/github/deweyjose/graphqlcodegen/Codegen.java
@@ -22,121 +22,122 @@ import com.netflix.graphql.dgs.codegen.Language;
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class Codegen extends AbstractMojo {
 
-    @Parameter(property = "schemaPaths", defaultValue = "${project.build.resources}/schema")
-    private File[] schemaPaths;
+	@Parameter(property = "schemaPaths", defaultValue = "${project.build.resources}/schema")
+	private File[] schemaPaths;
 
-    @Parameter(property = "packageName", defaultValue = "")
-    private String packageName;
+	@Parameter(property = "packageName", defaultValue = "")
+	private String packageName;
 
-    @Parameter(property = "typeMapping")
-    private Map typeMapping;
+	@Parameter(property = "typeMapping")
+	private Map<String, String> typeMapping;
 
-    @Parameter(property = "subPackageNameClient", defaultValue = "client")
-    private String subPackageNameClient;
+	@Parameter(property = "subPackageNameClient", defaultValue = "client")
+	private String subPackageNameClient;
 
-    @Parameter(property = "subPackageNameDatafetchers", defaultValue = "datafetchers")
-    private String subPackageNameDatafetchers;
+	@Parameter(property = "subPackageNameDatafetchers", defaultValue = "datafetchers")
+	private String subPackageNameDatafetchers;
 
-    @Parameter(property = "subPackageNameTypes", defaultValue = "types")
-    private String subPackageNameTypes;
+	@Parameter(property = "subPackageNameTypes", defaultValue = "types")
+	private String subPackageNameTypes;
 
-    @Parameter(property = "generateBoxedTypes", defaultValue = "false")
-    private boolean generateBoxedTypes;
+	@Parameter(property = "generateBoxedTypes", defaultValue = "false")
+	private boolean generateBoxedTypes;
 
-    @Parameter(property = "generateClient", defaultValue = "false")
-    private boolean generateClient;
+	@Parameter(property = "generateClient", defaultValue = "false")
+	private boolean generateClient;
 
-    @Parameter(property = "generateInterfaces", defaultValue = "false")
-    private boolean generateInterfaces;
+	@Parameter(property = "generateInterfaces", defaultValue = "false")
+	private boolean generateInterfaces;
 
-    @Parameter(property = "outputDir", defaultValue = "${project.build.directory}/generated-sources")
-    private File outputDir;
+	@Parameter(property = "outputDir", defaultValue = "${project.build.directory}/generated-sources")
+	private File outputDir;
 
-    @Parameter(property = "exampleOutputDir", defaultValue = "${project.build.directory}/generated-examples")
-    private File exampleOutputDir;
+	@Parameter(property = "exampleOutputDir", defaultValue = "${project.build.directory}/generated-examples")
+	private File exampleOutputDir;
 
-    @Parameter(property = "includeQueries")
-    private String[] includeQueries;
+	@Parameter(property = "includeQueries")
+	private String[] includeQueries;
 
-    @Parameter(property = "includeMutations")
-    private String[] includeMutations;
+	@Parameter(property = "includeMutations")
+	private String[] includeMutations;
 
-    @Parameter(property = "skipEntityQueries", defaultValue = "false")
-    private boolean skipEntityQueries;
+	@Parameter(property = "skipEntityQueries", defaultValue = "false")
+	private boolean skipEntityQueries;
 
-    @Parameter(property = "shortProjectionNames", defaultValue = "false")
-    private boolean shortProjectionNames;
+	@Parameter(property = "shortProjectionNames", defaultValue = "false")
+	private boolean shortProjectionNames;
 
-    @Parameter(property = "generateDataTypes", defaultValue = "true")
-    private boolean generateDataTypes;
+	@Parameter(property = "generateDataTypes", defaultValue = "true")
+	private boolean generateDataTypes;
 
-    @Parameter(property = "maxProjectionDepth", defaultValue = "10")
-    private int maxProjectionDepth;
+	@Parameter(property = "maxProjectionDepth", defaultValue = "10")
+	private int maxProjectionDepth;
 
-    @Parameter(property = "language", defaultValue = "java")
-    private String language;
+	@Parameter(property = "language", defaultValue = "java")
+	private String language;
 
-    @Parameter(property = "omitNullInputFields", defaultValue = "false")
-    private boolean omitNullInputFields;
+	@Parameter(property = "omitNullInputFields", defaultValue = "false")
+	private boolean omitNullInputFields;
 
-    @Parameter(property = "kotlinAllFieldsOptional", defaultValue = "false")
-    private boolean kotlinAllFieldsOptional;
+	@Parameter(property = "kotlinAllFieldsOptional", defaultValue = "false")
+	private boolean kotlinAllFieldsOptional;
 
-    @Parameter(property = "snakeCaseConstantNames", defaultValue = "false")
-    private boolean snakeCaseConstantNames;
+	@Parameter(property = "snakeCaseConstantNames", defaultValue = "false")
+	private boolean snakeCaseConstantNames;
 
-    private void verifySettings() {
-        if (Objects.isNull(packageName)) {
-            throw new RuntimeException("Please specify a packageName");
-        }
+	private void verifySettings() {
+		if (Objects.isNull(packageName)) {
+			throw new RuntimeException("Please specify a packageName");
+		}
 
-        if (schemaPaths.length != Arrays.stream(schemaPaths).collect(Collectors.toSet()).size()) {
-            throw new RuntimeException("Duplicate entries in schemaPaths");
-        }
+		if (schemaPaths.length != Arrays.stream(schemaPaths).collect(Collectors.toSet()).size()) {
+			throw new RuntimeException("Duplicate entries in schemaPaths");
+		}
 
-        for (final File schemaPath : schemaPaths) {
-            if (!schemaPath.exists()) {
-                try {
-                    throw new RuntimeException("Schema File: " + schemaPath.getCanonicalPath() + " does not exist!");
-                } catch (final IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		for (final File schemaPath : schemaPaths) {
+			if (!schemaPath.exists()) {
+				try {
+					throw new RuntimeException("Schema File: " + schemaPath.getCanonicalPath() + " does not exist!");
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        verifySettings();
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		verifySettings();
 
-        final CodeGenConfig config = new CodeGenConfig(
-                Collections.emptySet(),
-                Arrays.stream(schemaPaths).collect(Collectors.toSet()), 
-                outputDir.toPath(), 
-                exampleOutputDir.toPath(),
-                true, 
-                packageName, 
-                subPackageNameClient, 
-                subPackageNameDatafetchers, 
-                subPackageNameTypes,
-                Language.valueOf(language.toUpperCase()), 
-                generateBoxedTypes, 
-                generateClient, 
-                generateInterfaces,
-                typeMapping, 
-                Arrays.stream(includeQueries).collect(Collectors.toSet()),
-                Arrays.stream(includeMutations).collect(Collectors.toSet()), 
-                skipEntityQueries, 
-                shortProjectionNames,
-                generateDataTypes, 
-                omitNullInputFields, 
-                maxProjectionDepth, 
-                kotlinAllFieldsOptional,
-                snakeCaseConstantNames);
+		final CodeGenConfig config = new CodeGenConfig(
+				Collections.emptySet(),
+				Arrays.stream(schemaPaths).collect(Collectors.toSet()),
+				outputDir.toPath(),
+				exampleOutputDir.toPath(),
+				true,
+				packageName,
+				subPackageNameClient,
+				subPackageNameDatafetchers,
+				subPackageNameTypes,
+				Language.valueOf(language.toUpperCase()),
+				generateBoxedTypes,
+				generateClient,
+				generateInterfaces,
+				typeMapping,
+				Arrays.stream(includeQueries).collect(Collectors.toSet()),
+				Arrays.stream(includeMutations).collect(Collectors.toSet()),
+				skipEntityQueries,
+				shortProjectionNames,
+				generateDataTypes,
+				omitNullInputFields,
+				maxProjectionDepth,
+				kotlinAllFieldsOptional,
+				snakeCaseConstantNames
+			);
 
-        getLog().info(String.format("Codegen config: %n%s", config));
+		getLog().info(String.format("Codegen config: %n%s", config));
 
-        final CodeGen codeGen = new CodeGen(config);
-        codeGen.generate();
-    }
+		final CodeGen codeGen = new CodeGen(config);
+		codeGen.generate();
+	}
 }
