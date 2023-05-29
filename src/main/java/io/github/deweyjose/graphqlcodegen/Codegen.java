746cf4565f2e683a261ecb98e8eadc5c1e80857c
Zorglube <630192+zorglube@users.noreply.github.com>
2023-05-29 14:36:42 +0200

    Bump dgs-codegen => 5.8.0 (#78)
    * Bump dgs-codegen-core.version => 5.7.3
    
    * Bump dgs-codegen => 5.8.0
    
     - https://github.com/Netflix/dgs-codegen/pull/553
    
    ---------
    
    Co-authored-by: deweyjose <richard.e.jose@gmail.com>

--- src/main/java/io/github/deweyjose/graphqlcodegen/Codegen.java
+++ src/main/java/io/github/deweyjose/graphqlcodegen/Codegen.java
@@ -1,8 +1,22 @@
 package io.github.deweyjose.graphqlcodegen;
 
-import com.netflix.graphql.dgs.codegen.CodeGen;
-import com.netflix.graphql.dgs.codegen.CodeGenConfig;
-import com.netflix.graphql.dgs.codegen.Language;
+import static java.lang.String.format;
+import static java.util.Arrays.stream;
+import static java.util.Collections.emptySet;
+import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -12,188 +26,189 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-import java.util.Map.Entry;
-
-import static java.lang.String.format;
-import static java.util.Arrays.stream;
-import static java.util.Collections.emptySet;
-import static java.util.Objects.isNull;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
+import com.netflix.graphql.dgs.codegen.CodeGen;
+import com.netflix.graphql.dgs.codegen.CodeGenConfig;
+import com.netflix.graphql.dgs.codegen.Language;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class Codegen extends AbstractMojo {
 
-  @Parameter(defaultValue = "${project}")
-  private MavenProject project;
+	@Parameter(defaultValue = "${project}")
+	private MavenProject project;
 
-  @Parameter(property = "schemaPaths")
-  private File[] schemaPaths;
+	@Parameter(property = "schemaPaths")
+	private File[] schemaPaths;
 
-  @Parameter(alias = "schemaJarFilesFromDependencies", property = "schemaJarFilesFromDependencies")
-  private String[] schemaJarFilesFromDependencies;
-  private final List<File> schemaJarFilesFromDependenciesFiles = new ArrayList<>();
+	@Parameter(alias = "schemaJarFilesFromDependencies", property = "schemaJarFilesFromDependencies")
+	private String[] schemaJarFilesFromDependencies;
+	private final List<File> schemaJarFilesFromDependenciesFiles = new ArrayList<>();
 
-  @Parameter(property = "packageName", defaultValue = "")
-  private String packageName;
+	@Parameter(property = "packageName", defaultValue = "")
+	private String packageName;
 
-  @Parameter(property = "subPackageNameClient", defaultValue = "client")
-  private String subPackageNameClient;
+	@Parameter(property = "subPackageNameClient", defaultValue = "client")
+	private String subPackageNameClient;
 
-  @Parameter(property = "subPackageNameDatafetchers", defaultValue = "datafetchers")
-  private String subPackageNameDatafetchers;
+	@Parameter(property = "subPackageNameDatafetchers", defaultValue = "datafetchers")
+	private String subPackageNameDatafetchers;
 
-  @Parameter(property = "subPackageNameTypes", defaultValue = "types")
-  private String subPackageNameTypes;
+	@Parameter(property = "subPackageNameTypes", defaultValue = "types")
+	private String subPackageNameTypes;
 
-  @Parameter(property = "language", defaultValue = "java")
-  private String language;
+	@Parameter(property = "subPackageNameDocs", defaultValue = "docs")
+	private String subPackageNameDocs;
 
-  @Parameter(property = "typeMapping")
-  private Map<String, String> typeMapping;
+	@Parameter(property = "language", defaultValue = "java")
+	private String language;
 
-  @Parameter(property = "generateBoxedTypes", defaultValue = "false")
-  private boolean generateBoxedTypes;
+	@Parameter(property = "typeMapping")
+	private Map<String, String> typeMapping;
 
-  @Parameter(property = "generateClientApi", defaultValue = "false")
-  private boolean generateClientApi;
+	@Parameter(property = "generateBoxedTypes", defaultValue = "false")
+	private boolean generateBoxedTypes;
 
-  @Parameter(property = "generateClientApiV2", defaultValue = "false")
-  private boolean generateClientApiV2;
+	@Parameter(property = "generateClientApi", defaultValue = "false")
+	private boolean generateClientApi;
 
-  @Parameter(property = "generateDataTypes", defaultValue = "true")
-  private boolean generateDataTypes;
+	@Parameter(property = "generateClientApiV2", defaultValue = "false")
+	private boolean generateClientApiV2;
 
-  @Parameter(property = "generateInterfaces", defaultValue = "false")
-  private boolean generateInterfaces;
+	@Parameter(property = "generateDataTypes", defaultValue = "true")
+	private boolean generateDataTypes;
 
-  @Parameter(property = "generateKotlinNullableClasses", defaultValue = "false")
-  private boolean generateKotlinNullableClasses;
+	@Parameter(property = "generateInterfaces", defaultValue = "false")
+	private boolean generateInterfaces;
 
-  @Parameter(property = "generateKotlinClosureProjections", defaultValue = "false")
-  private boolean generateKotlinClosureProjections;
+	@Parameter(property = "generateKotlinNullableClasses", defaultValue = "false")
+	private boolean generateKotlinNullableClasses;
 
-  @Parameter(property = "outputDir", defaultValue = "${project.build.directory}/generated-sources")
-  private File outputDir;
+	@Parameter(property = "generateKotlinClosureProjections", defaultValue = "false")
+	private boolean generateKotlinClosureProjections;
 
-  @Parameter(property = "exampleOutputDir", defaultValue = "${project.build.directory}/generated-examples")
-  private File exampleOutputDir;
+	@Parameter(property = "outputDir", defaultValue = "${project.build.directory}/generated-sources")
+	private File outputDir;
 
-  @Parameter(property = "includeQueries")
-  private String[] includeQueries;
+	@Parameter(property = "exampleOutputDir", defaultValue = "${project.build.directory}/generated-examples")
+	private File exampleOutputDir;
 
-  @Parameter(property = "includeMutations")
-  private String[] includeMutations;
+	@Parameter(property = "includeQueries")
+	private String[] includeQueries;
 
-  @Parameter(property = "skipEntityQueries", defaultValue = "false")
-  private boolean skipEntityQueries;
+	@Parameter(property = "includeMutations")
+	private String[] includeMutations;
 
-  @Parameter(property = "shortProjectionNames", defaultValue = "false")
-  private boolean shortProjectionNames;
+	@Parameter(property = "skipEntityQueries", defaultValue = "false")
+	private boolean skipEntityQueries;
 
-  @Parameter(property = "maxProjectionDepth", defaultValue = "10")
-  private int maxProjectionDepth;
+	@Parameter(property = "shortProjectionNames", defaultValue = "false")
+	private boolean shortProjectionNames;
 
-  @Parameter(property = "omitNullInputFields", defaultValue = "false")
-  private boolean omitNullInputFields;
+	@Parameter(property = "maxProjectionDepth", defaultValue = "10")
+	private int maxProjectionDepth;
 
-  @Parameter(property = "kotlinAllFieldsOptional", defaultValue = "false")
-  private boolean kotlinAllFieldsOptional;
+	@Parameter(property = "omitNullInputFields", defaultValue = "false")
+	private boolean omitNullInputFields;
 
-  @Parameter(property = "snakeCaseConstantNames", defaultValue = "false")
-  private boolean snakeCaseConstantNames;
+	@Parameter(property = "kotlinAllFieldsOptional", defaultValue = "false")
+	private boolean kotlinAllFieldsOptional;
 
-  @Parameter(property = "writeToFiles", defaultValue = "true")
-  private boolean writeToFiles;
+	@Parameter(property = "snakeCaseConstantNames", defaultValue = "false")
+	private boolean snakeCaseConstantNames;
 
-  @Parameter(property = "includeSubscriptions")
-  private String[] includeSubscriptions;
+	@Parameter(property = "writeToFiles", defaultValue = "true")
+	private boolean writeToFiles;
 
-  @Parameter(property = "generateInterfaceSetters", defaultValue = "false")
-  private boolean generateInterfaceSetters;
+	@Parameter(property = "includeSubscriptions")
+	private String[] includeSubscriptions;
 
-  @Parameter(property = "generateInterfaceMethodsForInterfaceFields", defaultValue = "false")
-  private boolean generateInterfaceMethodsForInterfaceFields;
+	@Parameter(property = "generateInterfaceSetters", defaultValue = "false")
+	private boolean generateInterfaceSetters;
 
-  @Parameter(property = "javaGenerateAllConstructor", defaultValue = "false")
-  private boolean javaGenerateAllConstructor;
+	@Parameter(property = "generateInterfaceMethodsForInterfaceFields", defaultValue = "false")
+	private boolean generateInterfaceMethodsForInterfaceFields;
 
-  @Parameter(property = "implementSerializable", defaultValue = "false")
-  private boolean implementSerializable;
+	@Parameter(property = "generateDocs", defaultValue = "false")
+	private Boolean generateDocs;
 
-  @Parameter(property = "addGeneratedAnnotation", defaultValue = "false")
-  private boolean addGeneratedAnnotation;
+	@Parameter(property = "generatedDocsFolder", defaultValue = "./generated-docs")
+	private String generatedDocsFolder;
 
-  @Parameter(property = "addDeprecatedAnnotation", defaultValue = "false")
-  private boolean addDeprecatedAnnotation;
+	@Parameter(property = "javaGenerateAllConstructor", defaultValue = "false")
+	private boolean javaGenerateAllConstructor;
 
-  @Parameter(property = "dgs.codegen.skip", defaultValue = "false", required = false)
-  private boolean skip;
+	@Parameter(property = "implementSerializable", defaultValue = "false")
+	private boolean implementSerializable;
 
-  @Parameter(property = "generateCustomAnnotations", defaultValue = "false")
-  private boolean generateCustomAnnotations;
+	@Parameter(property = "addGeneratedAnnotation", defaultValue = "false")
+	private boolean addGeneratedAnnotation;
 
-  @Parameter(property = "includeImports")
-  private Map<String, String> includeImports;
+	@Parameter(property = "addDeprecatedAnnotation", defaultValue = "false")
+	private boolean addDeprecatedAnnotation;
 
-  @Parameter(property = "includeEnumImports")
-  private Map<String, Properties> includeEnumImports;
+	@Parameter(property = "dgs.codegen.skip", defaultValue = "false", required = false)
+	private boolean skip;
 
-  @Parameter(property = "includeClassImports")
-  private Map<String, Properties> includeClassImports;
+	@Parameter(property = "generateCustomAnnotations", defaultValue = "false")
+	private boolean generateCustomAnnotations;
 
-  private void verifySettings() {
-    if (isNull(packageName)) {
-      throw new RuntimeException("Please specify a packageName");
-    }
+	@Parameter(property = "includeImports")
+	private Map<String, String> includeImports;
 
-    int size = stream(schemaPaths).collect(toSet()).size();
-    if (schemaPaths.length != size) {
-      stream(schemaPaths).forEach(s -> {
-        getLog().error("schemaPath: " + s.getPath());
-      });
-      throw new RuntimeException("Duplicate entries in schemaPaths");
-    }
+	@Parameter(property = "includeEnumImports")
+	private Map<String, Properties> includeEnumImports;
 
+	@Parameter(property = "includeClassImports")
+	private Map<String, Properties> includeClassImports;
 
-    for (String jarDep : schemaJarFilesFromDependencies) {
-      String jarDepClean = jarDep.trim();
-      if (jarDepClean.isEmpty())
-        continue;
+	private void verifySettings() {
+		if (isNull(packageName)) {
+			throw new RuntimeException("Please specify a packageName");
+		}
 
-      Optional<Artifact> artifactOpt = findFromDependencies(jarDepClean);
-      if (artifactOpt.isPresent()) {
-        Artifact artifact = artifactOpt.get();
-        File file = artifact.getFile();
-        this.schemaJarFilesFromDependenciesFiles.add(file);
-      } else {
-        getLog().warn(String.format("Unable to find Artifact`%s`", jarDepClean));
-      }
+		final int size = stream(schemaPaths).collect(toSet()).size();
+		if (schemaPaths.length != size) {
+			stream(schemaPaths).forEach(s -> {
+				getLog().error("schemaPath: " + s.getPath());
+			});
+			throw new RuntimeException("Duplicate entries in schemaPaths");
+		}
 
-    }
+		for (final String jarDep : schemaJarFilesFromDependencies) {
+			final String jarDepClean = jarDep.trim();
+			if (jarDepClean.isEmpty()) {
+				continue;
+			}
 
-    for (final File schemaPath : schemaPaths) {
-      if (!schemaPath.exists()) {
-        try {
-          throw new RuntimeException(
-            format("Schema File: %s does not exist!", schemaPath.getCanonicalPath()));
-        } catch (final IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-  }
+			final Optional<Artifact> artifactOpt = findFromDependencies(jarDepClean);
+			if (artifactOpt.isPresent()) {
+				final Artifact artifact = artifactOpt.get();
+				final File file = artifact.getFile();
+				schemaJarFilesFromDependenciesFiles.add(file);
+			} else {
+				getLog().warn(String.format("Unable to find Artifact`%s`", jarDepClean));
+			}
 
-  @Override
-  public void execute() throws MojoExecutionException, MojoFailureException {
-    if (!skip) {
+		}
 
-      verifySettings();
+		for (final File schemaPath : schemaPaths) {
+			if (!schemaPath.exists()) {
+				try {
+					throw new RuntimeException(
+							format("Schema File: %s does not exist!", schemaPath.getCanonicalPath()));
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-      // @formatter:off
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		if (!skip) {
+
+			verifySettings();
+
+		// @formatter:off
       final CodeGenConfig config = new CodeGenConfig(
         emptySet(),
         stream(schemaPaths).collect(toSet()),
@@ -205,6 +220,7 @@ public class Codegen extends AbstractMojo {
         subPackageNameClient,
         subPackageNameDatafetchers,
         subPackageNameTypes,
+        subPackageNameDocs,
         Language.valueOf(language.toUpperCase()),
         generateBoxedTypes,
         generateClientApi,
@@ -225,6 +241,8 @@ public class Codegen extends AbstractMojo {
         snakeCaseConstantNames,
         generateInterfaceSetters,
         generateInterfaceMethodsForInterfaceFields,
+        generateDocs,
+        Paths.get(generatedDocsFolder),
         includeImports,
         includeEnumImports
           .entrySet()
@@ -248,23 +266,23 @@ public class Codegen extends AbstractMojo {
       );
       // @formatter:on
 
-      getLog().info(format("Codegen config: %n%s", config));
+			getLog().info(format("Codegen config: %n%s", config));
 
-      final CodeGen codeGen = new CodeGen(config);
-      codeGen.generate();
-    }
-  }
+			final CodeGen codeGen = new CodeGen(config);
+			codeGen.generate();
+		}
+	}
 
-  private Optional<Artifact> findFromDependencies(String artifactRef) {
-    String cleanRef = artifactRef.trim();
-    Set<Artifact> dependencyArtifacts = project.getDependencyArtifacts();
-    for (Artifact artifact : dependencyArtifacts) {
-      String ref = artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getVersion();
+	private Optional<Artifact> findFromDependencies(String artifactRef) {
+		final String cleanRef = artifactRef.trim();
+		final Set<Artifact> dependencyArtifacts = project.getDependencyArtifacts();
+		for (final Artifact artifact : dependencyArtifacts) {
+			final String ref = artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getVersion();
 
-      if (ref.equals(cleanRef)) {
-        return Optional.of(artifact);
-      }
-    }
-    return Optional.empty();
-  }
+			if (ref.equals(cleanRef)) {
+				return Optional.of(artifact);
+			}
+		}
+		return Optional.empty();
+	}
 }
