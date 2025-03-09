b74e8bc669b51cd5b253e0be57fd6c78f6834f90
deweyjose <richard.e.jose@gmail.com>
2025-03-09 10:48:58 -0400

    spotless integration (#193)
    * apply spotless
    
    * integrate spotless
    
    * integrate spotless
    
    * test spotless failure
    
    * fix spotless error

--- src/main/java/io/github/deweyjose/graphqlcodegen/Properties.java
+++ src/main/java/io/github/deweyjose/graphqlcodegen/Properties.java
@@ -1,23 +1,21 @@
 package io.github.deweyjose.graphqlcodegen;
 
+import java.util.Map;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.util.Map;
-
 public class Properties {
-    @Parameter
-    private Map<String, String> properties;
+  @Parameter private Map<String, String> properties;
 
-    @Override
-    public String toString() {
-        return properties.toString();
-    } // to test
+  @Override
+  public String toString() {
+    return properties.toString();
+  } // to test
 
-    public Map<String, String> getProperties() {
-        return properties;
-    }
+  public Map<String, String> getProperties() {
+    return properties;
+  }
 
-    public void setProperties(final Map<String, String> properties) {
-        this.properties = properties;
-    }
+  public void setProperties(final Map<String, String> properties) {
+    this.properties = properties;
+  }
 }
