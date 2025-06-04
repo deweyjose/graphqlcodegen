037ddb98b555ed3e57d6722c84497165083a0a80
deweyjose <richard.e.jose@gmail.com>
2025-06-03 20:53:17 -0400

    Replace maven-project with maven-core 3.9.9 and update plugin version (#242)
    * chore: replace maven-project with maven-core 3.9.9, update plugin version, and fix test imports
    
    * chore: trigger whitespace build
    
    ---------
    
    Co-authored-by: Dewey jose <dewjose@Deweys-MacBook-Pro.local>

--- graphqlcodegen-maven-plugin/src/test/java/io/github/deweyjose/graphqlcodegen/services/SchemaFileServiceTest.java
+++ graphqlcodegen-maven-plugin/src/test/java/io/github/deweyjose/graphqlcodegen/services/SchemaFileServiceTest.java
@@ -1,7 +1,7 @@
 package io.github.deweyjose.graphqlcodegen.services;
 
-import static junit.framework.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
